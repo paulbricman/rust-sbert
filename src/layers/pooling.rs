9a69515d821042b3b40922626f7f14d1c36617cd
paulbricman <paulbricman@protonmail.com>
2021-06-30 15:15:59 +0200

    Update to tch 0.5

--- src/layers/pooling.rs
+++ src/layers/pooling.rs
@@ -37,9 +37,9 @@ impl Pooling {
         const DIM: [i64; 1] = [1];
 
         let mut sum_mask = input_mask_expanded.copy();
-        sum_mask = sum_mask.sum1(&DIM, false, Kind::Float);
+        sum_mask = sum_mask.sum_dim_intlist(&DIM, false, Kind::Float);
         let sum_embeddings =
-            (token_embeddings * input_mask_expanded).sum1(&DIM, false, Kind::Float);
+            (token_embeddings * input_mask_expanded).sum_dim_intlist(&DIM, false, Kind::Float);
 
         output_vectors.push(sum_embeddings / sum_mask);
 
